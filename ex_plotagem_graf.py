bc70ed3a42b179673cf9f23a2e76255c50017b9c
Lopes, Maycon B <lopes.maycon@gmail.com>
2022-04-09 08:50:35 -0300

    Initial commit

---
+++ ex_plotagem_graf.py
@@ -0,0 +1,20 @@
+"""Exemplo de plotagem de gráfico utilizando o matplotlib"""
+
+import matplotlib.pyplot as plt
+
+notas_matematica = ['Matemática', 8, 7, 6, 6, 7, 7, 8, 10]
+notas_portugues = ['Português', 9, 9, 9, 8, 5, 6, 8, 5]
+notas_geografia = ['Geografia', 10, 10, 6, 7, 7, 7, 8, 7]
+notas = [notas_matematica, notas_portugues, notas_geografia]
+
+for nota in notas:
+
+    x = list(range(1, 9))
+    y = nota[1:]
+    plt.plot(x, y, marker='o', label=nota[0])
+    plt.xlabel('Provas')
+    plt.ylabel('Notas')
+    plt.title('Gráfico com as notas')
+    plt.legend()
+
+plt.show()
